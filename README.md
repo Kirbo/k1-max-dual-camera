8193773291becd28c210eb14d52df2b4e955d0a7
Kimmo Saari <kimmo.saari@fennicagaming.com>
2023-10-05 00:43:36 +0300

    refactor: slightly refactoring

---
+++ README.md
@@ -0,0 +1,5 @@
+# Creality K1 Max - Dual webcams
+
+```sh
+(cd && git clone https://gitlab.com/kirbo/k1-max-dual-camera.git && cd k1-max-dual-camera && ./install.sh)
+```
--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-cp startup_dual_cameras.sh /usr/data/startup_dual_cameras.sh
+cp script/startup_dual_cameras.sh /usr/data/startup_dual_cameras.sh
 chmod +x /usr/data/startup_dual_cameras.sh
 
-cp S99dual_cameras /etc/init.d/S99dual_cameras
+cp service/S99dual_cameras /etc/init.d/S99dual_cameras
 chmod +x /etc/init.d/S99dual_cameras
 
 /etc/init.d/S99dual_cameras start
